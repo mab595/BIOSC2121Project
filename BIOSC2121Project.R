4b57e443a4a6ce8cf5f63f931a1360a005769f80
Matt Borrelli <mab595@pitt.edu>
2019-12-02 16:19:19 -0500

    Completed statistical analysis for pairwise testing following Anova, added R script output text file, plot png file, and first draft of the project paper

--- BIOSC2121Project.R
+++ BIOSC2121Project.R
@@ -1,6 +1,13 @@
 rm(list=ls()) #clear all variables before beginning
 #ctrl + L clears console
 
+# Start writing to an output file
+sink('analysis-output.txt')
+
+set.seed(12345)
+x <-rnorm(10,10,1)
+y <-rnorm(10,11,1)
+
 circadian <- read.csv(file="C:/Users/borre/Desktop/backup/Grad School/Coursework/BIOSC 2121 - Biostats/Project/Project_Data2.csv")
 
 circadian$Treatment <- factor(circadian$treatment, 
@@ -8,12 +15,14 @@ circadian$Treatment <- factor(circadian$treatment,
                                          "IL-13 (10ng/mL)","IL-13 (50ng/mL)", "IL-13 (100ng/mL)", "IL-33 (100ng/mL)", 
                                          "UV (30 mins)", "Hypoxia Control")) 
 
-meanCount <- tapply(circadian$shift, circadian$treatment, mean)
-sdevCount <- tapply(circadian$shift, circadian$treatment, sd)
-n         <- tapply(circadian$shift, circadian$treatment, length)
+meanCount <- tapply(circadian$count, circadian$treatment, mean)
+sdevCount <- tapply(circadian$count, circadian$treatment, sd)
+n         <- tapply(circadian$count, circadian$treatment, length)
 data.frame(mean = meanCount, std.dev = sdevCount, n = n)
 
-stripchart(shift ~ treatment, data = circadian, method = "jitter", vertical = TRUE)
+png("ProjChart.png",width = 720, height=480)
+
+stripchart(count ~ treatment, data = circadian, method = "jitter", vertical = TRUE)
 
 seCount <- sdevCount / sqrt(n)
 adjustAmount <- 0.2
@@ -21,10 +30,28 @@ segments( c(1,2,3,4,5) + adjustAmount, meanCount - seCount,
           c(1,2,3,4,5) + adjustAmount, meanCount + seCount )
 points(meanCount ~ c( c(1,2,3,4,5) + adjustAmount ))
 
-circadianAnova <- lm(shift ~ treatment, data = circadian)
+dev.off()
+
+circadianAnova <- lm(count ~ treatment, data = circadian)
 anova(circadianAnova)
 
 circadianAnovaSummary <- summary(circadianAnova)
 circadianAnovaSummary$r.squared
 
-kruskal.test(shift ~ treatment, data = circadian)
+#kruskal.test(count ~ treatment, data = circadian)
+
+P_adj=p.adjust(.05, "bonferroni",n=10)
+
+pairwise.t.test(circadian$count,circadian$treatment, p.adj = "bonferroni")
+pairwise.t.test(circadian$count,circadian$treatment, p.adj="holm")
+
+
+
+# Stop writing to the file
+sink()
+
+
+# Append to the file
+sink('analysis-output.txt', append=TRUE)
+cat("Some more stuff here...\n")
+sink()
